00fecbfe6d0fb7e59afeea7e43e396bcc6290ec9
webdevyoo <yjk7454@naver.com>
2025-05-25 22:04:31 +0900

    feat: `App.js` Text 컴포넌트 style-sheet create color red 적용

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello</Text>
       <Text>Hello! I made a RN App</Text>
       <StatusBar style="auto" />
     </View>
@@ -18,4 +19,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 28,
+    color: 'red'
+  }
 });
